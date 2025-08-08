cae363b7b48e2c0d4b3a47f545f449c3030099d5
InhaHwang35 <rachelinha7@gmail.com>
2025-08-08 23:33:00 +0900

    Delete fullproject directory bc of frontendtoto is not here

--- fullproject/backendtoto/toto/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Team, Member, Task
-
-admin.site.register(Team)
-admin.site.register(Member)
-admin.site.register(Task)
-# Register your models here.
